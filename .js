8617509d227045866b40382db36523c9c343eeae
EmeraldNZ <ahyuancy@gmail.com>
2018-12-31 16:19:58 +1300

    Update .js

--- .js
+++ .js
@@ -4,13 +4,13 @@ function uniteUnique(arr) {
     let allArr = [...arguments];
   
     for(let i = 1; i < allArr.length; i++){
-    allArr[i].forEach(function(x){
-        if(!arr.includes(x)){
-            arr.push(x);
-        }
-    })
+        allArr[i].forEach(function(x){
+            if(!arr.includes(x)){
+                arr.push(x);
+            }
+        })
     }
-   return arr;  
+    return arr;  
 }
 uniteUnique([1, 3, 2], [1, [3]], [2, [4]]); // Return [1, 3, 2, [5], [4]]
 
@@ -22,10 +22,10 @@ function uniteUnique(...arrays) {
 
   // make an array out of the given arrays and flatten it using the spread operator
     
-  const flatArray = [].concat(...arrays); // Same as const flatArray = [].concat(...arguments);
+   const flatArray = [].concat(...arrays); // Same as const flatArray = [].concat(...arguments);
 
   // use Set to remove duplicate elements from flatArray
-  return [...new Set(flatArray)];
+   return [...new Set(flatArray)];
 }
 
 
